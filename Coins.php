cd8b066d2bb99bfff6149514d253c7b816f66409
gokturkturan <89880316+gokturkturan@users.noreply.github.com>
2022-01-31 15:41:52 +0300

    Update Coins.php

--- Coins.php
+++ Coins.php
@@ -30,32 +30,32 @@
 		<h3><u>Piyasa Değeri En Yüksek İlk 20 Coin</u></h3>
 		<table>
   			<tr>
-    				<td><b><img src="coins/1.png"><a href="https://coinmarketcap.com/tr/currencies/bitcoin/">1. Bitcoin</a></b></td>
-				<td><b><img src="coins/1027.png"><a href="https://coinmarketcap.com/tr/currencies/ethereum/">2. Ethereum</a></b></td>
-				<td><b><img src="coins/1839.png"><a href="https://coinmarketcap.com/tr/currencies/binance-coin/">3. Binance Coin</a></b></td>
-				<td><b><img src="coins/825.png"><a href="https://coinmarketcap.com/tr/currencies/tether/">4. Tether</a></b></td>
-				<td><b><img src="coins/5426.png"><a href="https://coinmarketcap.com/tr/currencies/solana/">5. Solana</a></b></td>
+    			<td><b><img src="coins/1.png"><a href="bitcoin.php">1. Bitcoin</a></b></td>
+				<td><b><img src="coins/1027.png"><a href="ethereum.php">2. Ethereum</a></b></td>
+				<td><b><img src="coins/825.png"><a href="tether.php">3. Tether</a></b></td>
+				<td><b><img src="coins/1839.png"><a href="bnb.php">4. Binance Coin</a></b></td>
+				<td><b><img src="coins/3408.png"><a href="usdc.php">5. USD Coin</a></b></td>
   			</tr>
   			<tr>
-    				<td><b><img src="coins/2010.png"><a href="https://coinmarketcap.com/tr/currencies/cardano/">6. Cordano</a></b></td>
-    				<td><b><img src="coins/52.png"><a href="https://coinmarketcap.com/tr/currencies/xrp/">7. XRP</a></b></td>
-    				<td><b><img src="coins/3408.png"><a href="https://coinmarketcap.com/tr/currencies/usd-coin/">8. USD Coin</a></b></td>
-				<td><b><img src="coins/6636.png"><a href="https://coinmarketcap.com/tr/currencies/polkadot-new/">9. Polkadot</a></b></td>
-				<td><b><img src="coins/74.png"><a href="https://coinmarketcap.com/tr/currencies/dogecoin/">10. Doge Coin</a></b></td>
+  				<td><b><img src="coins/2010.png"><a href="ada.php">6. Cordano</a></b></td>
+    				<td><b><img src="coins/5426.png"><a href="sol.php">7. Solana</a></b></td>
+    				<td><b><img src="coins/52.png"><a href="xrp.php">8. XRP</a></b></td>
+    				<td><b><img src="coins/4172.png"><a href="terra.php">9. Terra</a></b></td>
+    				<td><b><img src="coins/74.png"><a href="doge.php">10. Doge Coin</a></b></td>
   			</tr>
   			<tr>
-    				<td><b><img src="coins/5805.png"><a href="https://coinmarketcap.com/tr/currencies/avalanche/">11. Avalanche</a></b></td>
-    				<td><b><img src="coins/5994.png"><a href="https://coinmarketcap.com/tr/currencies/shiba-inu/">12. SHIBA INU</a></b></td>
-    				<td><b><img src="coins/4172.png"><a href="https://coinmarketcap.com/tr/currencies/terra-luna/">13. Terra</a></b></td>
-				<td><b><img src="coins/3635.png"><a href="https://coinmarketcap.com/tr/currencies/crypto-com-coin/">14. Crypto.com Coin</a></b></td>
-				<td><b><img src="coins/3717.png"><a href="https://coinmarketcap.com/tr/currencies/wrapped-bitcoin/">15. Wrapped Bitcoin</a></b></td>
+  				<td><b><img src="coins/6636.png"><a href="polkadot.php">11. Polkadot</a></b></td>
+    				<td><b><img src="coins/5805.png"><a href="avax.php">12. Avalanche</a></b></td>
+    				<td><b><img src="coins/4687.png"><a href="busd.php">13. Binance USD</a></b></td>
+    				<td><b><img src="coins/3890.png" width="65"><a href="matic.php">14. Polygon</a></b></td>
+    				<td><b><img src="coins/5994.png"><a href="shib.php">15. SHIBA INU</a></b></td>
   			</tr>
 			<tr>
-    				<td><b><img src="coins/2.png"><a href="https://coinmarketcap.com/tr/currencies/litecoin/">16. Litecoin</a></b></td>
-				<td><b><img src="coins/4687.png"><a href="https://coinmarketcap.com/tr/currencies/binance-usd/">17. Binance USD</a></b></td>
-				<td><b><img src="coins/7083.png"><a href="https://coinmarketcap.com/tr/currencies/uniswap/">18. Uniswap</a></b></td>
-				<td><b><img src="coins/3890.png" width="65"><a href="https://coinmarketcap.com/tr/currencies/polygon/">19. Polygon</a></b></td>
-				<td><b><img src="coins/1975.png"><a href="https://coinmarketcap.com/tr/currencies/chainlink/">20. Chainlink</a></b></td>
+				<td><b><img src="coins/3635.png"><a href="crypto.php">16. Crypto.com Coin</a></b></td>
+				<td><b><img src="coins/3717.png"><a href="wbtc.php">17. Wrapped Bitcoin</a></b></td>
+    				<td><b><img src="coins/2.png"><a href="ltc.php">18. Litecoin</a></b></td>
+				<td><b><img src="coins/7083.png"><a href="uni.php">19. Uniswap</a></b></td>
+				<td><b><img src="coins/1975.png"><a href="link.php">20. Chainlink</a></b></td>
   			</tr>
 		</table>
 		
@@ -91,32 +91,32 @@
 		<h3><u>Piyasa Değeri En Yüksek İlk 20 Coin</u></h3>
 		<table>
   			<tr>
-    				<td><b><img src="coins/1.png"><a href="https://coinmarketcap.com/tr/currencies/bitcoin/">1. Bitcoin</a></b></td>
-				<td><b><img src="coins/1027.png"><a href="https://coinmarketcap.com/tr/currencies/ethereum/">2. Ethereum</a></b></td>
-				<td><b><img src="coins/1839.png"><a href="https://coinmarketcap.com/tr/currencies/binance-coin/">3. Binance Coin</a></b></td>
-				<td><b><img src="coins/825.png"><a href="https://coinmarketcap.com/tr/currencies/tether/">4. Tether</a></b></td>
-				<td><b><img src="coins/5426.png"><a href="https://coinmarketcap.com/tr/currencies/solana/">5. Solana</a></b></td>
+    			<td><b><img src="coins/1.png"><a href="bitcoin.php">1. Bitcoin</a></b></td>
+				<td><b><img src="coins/1027.png"><a href="ethereum.php">2. Ethereum</a></b></td>
+				<td><b><img src="coins/825.png"><a href="tether.php">3. Tether</a></b></td>
+				<td><b><img src="coins/1839.png"><a href="bnb.php">4. Binance Coin</a></b></td>
+				<td><b><img src="coins/3408.png"><a href="usdc.php">5. USD Coin</a></b></td>
   			</tr>
   			<tr>
-    				<td><b><img src="coins/2010.png"><a href="https://coinmarketcap.com/tr/currencies/cardano/">6. Cordano</a></b></td>
-    				<td><b><img src="coins/52.png"><a href="https://coinmarketcap.com/tr/currencies/xrp/">7. XRP</a></b></td>
-    				<td><b><img src="coins/3408.png"><a href="https://coinmarketcap.com/tr/currencies/usd-coin/">8. USD Coin</a></b></td>
-				<td><b><img src="coins/6636.png"><a href="https://coinmarketcap.com/tr/currencies/polkadot-new/">9. Polkadot</a></b></td>
-				<td><b><img src="coins/74.png"><a href="https://coinmarketcap.com/tr/currencies/dogecoin/">10. Doge Coin</a></b></td>
+  				<td><b><img src="coins/2010.png"><a href="ada.php">6. Cordano</a></b></td>
+    				<td><b><img src="coins/5426.png"><a href="sol.php">7. Solana</a></b></td>
+    				<td><b><img src="coins/52.png"><a href="xrp.php">8. XRP</a></b></td>
+    				<td><b><img src="coins/4172.png"><a href="terra.php">9. Terra</a></b></td>
+    				<td><b><img src="coins/74.png"><a href="doge.php">10. Doge Coin</a></b></td>
   			</tr>
   			<tr>
-    				<td><b><img src="coins/5805.png"><a href="https://coinmarketcap.com/tr/currencies/avalanche/">11. Avalanche</a></b></td>
-    				<td><b><img src="coins/5994.png"><a href="https://coinmarketcap.com/tr/currencies/shiba-inu/">12. SHIBA INU</a></b></td>
-    				<td><b><img src="coins/4172.png"><a href="https://coinmarketcap.com/tr/currencies/terra-luna/">13. Terra</a></b></td>
-				<td><b><img src="coins/3635.png"><a href="https://coinmarketcap.com/tr/currencies/crypto-com-coin/">14. Crypto.com Coin</a></b></td>
-				<td><b><img src="coins/3717.png"><a href="https://coinmarketcap.com/tr/currencies/wrapped-bitcoin/">15. Wrapped Bitcoin</a></b></td>
+  				<td><b><img src="coins/6636.png"><a href="polkadot.php">11. Polkadot</a></b></td>
+    				<td><b><img src="coins/5805.png"><a href="avax.php">12. Avalanche</a></b></td>
+    				<td><b><img src="coins/4687.png"><a href="busd.php">13. Binance USD</a></b></td>
+    				<td><b><img src="coins/3890.png" width="65"><a href="matic.php">14. Polygon</a></b></td>
+    				<td><b><img src="coins/5994.png"><a href="shib.php">15. SHIBA INU</a></b></td>
   			</tr>
 			<tr>
-    				<td><b><img src="coins/2.png"><a href="https://coinmarketcap.com/tr/currencies/litecoin/">16. Litecoin</a></b></td>
-				<td><b><img src="coins/4687.png"><a href="https://coinmarketcap.com/tr/currencies/binance-usd/">17. Binance USD</a></b></td>
-				<td><b><img src="coins/7083.png"><a href="https://coinmarketcap.com/tr/currencies/uniswap/">18. Uniswap</a></b></td>
-				<td><b><img src="coins/3890.png" width="65"><a href="https://coinmarketcap.com/tr/currencies/polygon/">19. Polygon</a></b></td>
-				<td><b><img src="coins/1975.png"><a href="https://coinmarketcap.com/tr/currencies/chainlink/">20. Chainlink</a></b></td>
+				<td><b><img src="coins/3635.png"><a href="crypto.php">16. Crypto.com Coin</a></b></td>
+				<td><b><img src="coins/3717.png"><a href="wbtc.php">17. Wrapped Bitcoin</a></b></td>
+    				<td><b><img src="coins/2.png"><a href="ltc.php">18. Litecoin</a></b></td>
+				<td><b><img src="coins/7083.png"><a href="uni.php">19. Uniswap</a></b></td>
+				<td><b><img src="coins/1975.png"><a href="link.php">20. Chainlink</a></b></td>
   			</tr>
 		</table>
 		
